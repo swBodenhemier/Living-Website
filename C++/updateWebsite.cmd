b0a375167c805a0b26383c73a99872d3b37543a3
jk2429 <149831717+jk2429@users.noreply.github.com>
2024-04-26 18:44:16 -0700

    Delete C++/updateWebsite.cmd

--- C++/updateWebsite.cmd
+++
@@ -1,9 +0,0 @@
-rem Change directories to deployed_website
-cd ../../../deployed_website
-
-rem Commit HTML file changes
-echo Pushing updated files to GitHub
-git checkout pending_changes
-git add *.html
-git commit -m "Update navigation bar. AUTOMATIC UPDATE: Check forr errors before merging."
-git push origin
