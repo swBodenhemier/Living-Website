9efde8a469690159728e24dd0d0f676eebfdcaea
Carter K <101891881+Amazn1234@users.noreply.github.com>
2024-04-27 13:08:13 -0700

    Update downloadDatabase.bat

--- C++/downloadDatabase.bat
+++ C++/downloadDatabase.bat
@@ -1,9 +1,9 @@
 REM MySQL Connection Details
-set DB_HOST=mysql.ac.nau.edu
+set DB_HOST=
 set DB_PORT=3306
-set DB_USER=livingwebsite
-set DB_PASS=Vv_dckUVIn
-set DB_NAME=livingwebsite
+set DB_USER=
+set DB_PASS=
+set DB_NAME=
 
 REM MySQL Query
 set QUERY=SELECT * FROM Object;
@@ -15,7 +15,6 @@ REM Change directories
 cd Database Files
 
 REM MySQL Command
-rem mysql -h mysql.ac.nau.edu --port=3306 -u livingwebsite -pVv_dckUVIn -D livingwebsite -e "SELECT * FROM Object;" | sed "s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//" > output.csv
 mysql -h %DB_HOST% --port=%DB_PORT% -u %DB_USER% -p%DB_PASS% -D %DB_NAME% -e "%QUERY%" | sed s/\t/","/g;s/^/"/;s/$/"/;s/\n//g > output.csv
 
 REM Change directories back to original
