0a1f7a6b96fcacca6530a1064a59a04e38e9a262
jk2429 <149831717+jk2429@users.noreply.github.com>
2024-04-26 18:44:28 -0700

    Delete C++/setup.cmd

--- C++/setup.cmd
+++
@@ -1,7 +0,0 @@
-rem Set up all the directories to be ready to download databse items, html files and upload directory to github
-if not exist "HTMLFiles" (mkdir "HTMLFiles")
-if not exist "deployed_website" (git clone https://github.com/jk2429/deployed_website)
-if not exist "Database Files" (mkdir "Database Files")
-
-rem Download HTML files to update into HTMLFiles folder
-updateHTMLFiles.cmd
